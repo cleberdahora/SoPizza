ee0acbbbd8f3a49690d26d66450daa9af9283e77
unknown <cleberdahora@hotmail.com>
2015-06-30 11:55:55 -0300

    make it better

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
- var express  = require('express');
-    var app      = express();                               // create our app w/ express
-    var mongoose = require('mongoose');                     // mongoose for mongodb
-    var morgan = require('morgan');             // log requests to the console (express4)
-    var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
-    var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+var express = require('express');
+var app = express();
 
-    // configuration =================
-    mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');     // connect to mongoDB database on modulus.io
+app.set('port', (process.env.PORT || 5000));
 
-    app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
-    app.use(morgan('dev'));                                         // log every request to the console
-    app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-    app.use(bodyParser.json());                                     // parse application/json
-    app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-    app.use(methodOverride());
+app.use(express.static(__dirname + '/public'));
 
-    // listen (start app with node server.js) ======================================
-    app.listen(3000);
-    console.log("App listening on port 3000");
+// views is directory for all template files
+app.set('public_html', __dirname + '/public_html');
+app.set('public_html engine', 'html');
+
+app.get('/', function(request, response) {
+  response.render('index')
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
