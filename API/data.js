9fd705f1c4ca5c0a4e7445e613650c77b6c59cad
Asus <cleberdahora@hotmail.com>
2015-07-13 22:17:50 -0300

    New Comit

--- API/data.js
+++
@@ -1,9 +0,0 @@
-
-req.getConnection(function(err,connection){
- connection.query('SELECT * FROM dados',[],function(err,result){
- if(err) return res.status(400).json(err);
- 
- return res.status(200).json(result);
- 
- });
-});
--- server.js
+++ server.js
@@ -1,30 +1,47 @@
 var express = require('express');
 var app = express();
+var routes = require('routes');
 var mysql = require('mysql');
-var connection  = require('express-myconnection');
+
+var database = "sopizza";
+var table = "dados";
+
+var client = mysql.createClient({
+    user: 'root',
+    password: 'cletha415623',
+    host: 'localhost',
+    port: 3306
+});
+
+client.query('USE' + database);
+
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-app.use(
-   connection(mysql,{
-     host: 'localhost',
-     user: 'root',
-     password : 'cletha415623',
-     port : 3306, //port mysql
-     database:'api'
-   },'request')
-);
-
 // views is directory for all template files
 app.set('public_html', __dirname + '/public_html');
 app.set('public_html engine', 'html');
 
-app.get('/', function(request, response) {
-  response.render('index')
+app.get('/', function (request, response) {
+    response.render('index')
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.get('/dados', function (request, res){
+
+
+    client.query(
+           'SELECT * FROM'+ table,
+    function(err, results, fields){
+        if(err){
+            throw err;
+    }
+        res.send(results);   
+    });
+            
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
 });
